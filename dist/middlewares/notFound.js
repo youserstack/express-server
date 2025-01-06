62a98e9545cc8bcbcbda4743f8516a09c80ec9b3
youserstack <youserstack@gmail.com>
2025-01-06 21:15:27 +0900

    update

--- dist/middlewares/notFound.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const notFound = (req, res, next) => {
-    const error = new Error("Not Found");
-    error.status = 404;
-    next(error);
-};
-exports.default = notFound;
